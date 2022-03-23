57106515b4d9b3ab8d9ce123203f34aaa21f1173
veruoz <0688691@gmail.com>
2022-03-23 09:11:21 +0300

    PostController

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # react-blog-mern-mongo
 Blog NodeJS + Express + MongoDB + ReactJS
 
+- Установка MongoDB, Mongosh (добавление Path)
+- Подключение MongoDB, Mongoose (connect with MongoDB), Express, Babel, body-parser (read post data)
+- Регистрация в MongoDB и создание Cluster, БД + Collections, user (pass, login)
+- url для подключения (указание в ссылке login, pass, DB)
+- создание PostModel используя Schema Mongoose(шаблон для добавления записей в DB), маршрутов
+- Тестирование в Postman get, post, delete, put
+
+- Создание контроллера PostController с методами
+
 ## Start server
 mongosh
 
-## Start Node.js
+## Start Node.js Babel
 yarn build-start
+
+## Testing Postman & MongoDB cloud
+http://localhost:333/posts
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import PostModel from './models/Post'
+import PostController from './controllers/PostController'
+const Post = new PostController()
 // // позволяет читать данные через метод post
 import bodyParser from "body-parser";
 import express from "express";
 
 const app = express()
-
 mongoose.connect('mongodb+srv://user_01:NTYJ56eew@cluster0.ur1r9.mongodb.net/node-blog?retryWrites=true&w=majority')
     .then((res) => console.log('Connected'))
     .catch((error) => console.log(error))
@@ -13,50 +13,12 @@ mongoose.connect('mongodb+srv://user_01:NTYJ56eew@cluster0.ur1r9.mongodb.net/nod
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-// // маршрут
-app.post('/posts', (req, res) => {
-
-    const data = req.body
-
-    const post = new PostModel({
-        title: data.title,
-        text: data.text
-    })
-
-    post.save().then(() => {
-        res.send({status: 'ok'})
-    })
-})
-
-app.get('/posts', (req, res) => {
-    PostModel.find().then((err, posts) => {
-        if (err) {
-            return res.send(err)
-        }
-        res.json(posts)
-    })
-})
-
-app.delete('/posts/:id', (req, res) => {
-    PostModel.remove({
-        _id: req.params.id
-    }).then(post => {
-        if(post) {
-            res.json({status: 'deleted'})
-        } else {
-            res.json({status: 'error'})
-        }
-    })
-})
-
-app.put('/posts/:id', (req, res) => {
-    PostModel.findByIdAndUpdate(req.params.id, {$set: req.body}, err => {
-        if(err) {
-            res.send(err)
-        }
-        res.json({status: 'updated'})
-    })
-})
+// маршруты
+app.get('/posts', Post.index)
+app.post('/posts', Post.create)
+app.get('/posts/:id', Post.read)
+app.delete('/posts/:id', Post.delete)
+app.put('/posts/:id', Post.update)
 
 
 // локальный путь http://localhost:333/posts
